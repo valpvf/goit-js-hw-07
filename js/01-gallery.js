bc5956e28b4e4bd5e32a4d9b309a8a54bc0f252a
Valerii Pozhydaiev <p_vf@i.ua>
2023-03-26 14:51:52 +0300

    correct insertig el on site

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,6 +5,9 @@ const refs = {
   gallery: document.querySelector(".gallery"),
 };
 
+const onMarkupGallery = (galleryItems) =>
+  galleryItems.map(onCreateImg).join("");
+
 const onCreateImg = ({ preview, original, description }) => {
   return `
     <li class="gallery__item">
@@ -20,32 +23,33 @@ const onCreateImg = ({ preview, original, description }) => {
     `;
 };
 
-const onMarkupGallery = (galleryItems) =>
-  galleryItems.map(onCreateImg).join("");
-
-refs.gallery.innerHTML = onMarkupGallery(galleryItems);
+refs.gallery.insertAdjacentHTML(
+  "afterbegin",
+  onMarkupGallery(galleryItems)
+);
 
 refs.gallery.addEventListener("click", onGalleryClick);
 
 function onGalleryClick(e) {
+  e.preventDefault();
+
   if (e.target.nodeName !== "IMG") {
     return;
   }
-  e.preventDefault();
 
   basicLightbox
     .create(
       `<img class="gallery__image"
-              src="${e.target.dataset.source}"
-              alt="${e.target.getAttribute("alt")}"/>`
+        src="${e.target.dataset.source}"
+        alt="${e.target.getAttribute("alt")}"/>`
     )
     .show();
-  // document.addEventListener("keydown", onModalEscClose);
+  document.addEventListener("keydown", onModalEscClose);
 }
 
-// function onModalEscClose(e) {
-//   if (e.code === "Escape") {
-//     instance.close();
-// refs.gallery.removeEventListener("click", onGalleryClick);
-//   }
-// }
+function onModalEscClose(e) {
+  if (e.code === "Escape") {
+    console.log(document.querySelector(".basicLightbox").remove());
+    document.removeEventListener("keydown", onModalEscClose);
+  }
+}
